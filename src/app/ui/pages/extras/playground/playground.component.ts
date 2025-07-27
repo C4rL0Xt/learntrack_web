251e4267494ec18812f7f3ae2cb16cffbed55e04
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2025-07-27 16:19:57 -0500

    Agrega toogle button

--- src/app/ui/pages/extras/playground/playground.component.ts
+++ src/app/ui/pages/extras/playground/playground.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { SeccionComponent } from './components/seccion-component/seccion.component';
 import { TrackuiToggleButton } from '../../../shared/trackui/trackui-toggle-button/trackui-toggle-button.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'playground',
-  imports: [SeccionComponent, TrackuiToggleButton],
+  imports: [SeccionComponent, ReactiveFormsModule, TrackuiToggleButton],
   templateUrl: 'playground.component.html',
   styleUrl: 'playground.component.scss',
 })
-export class PlayGroundPage {}
+export class PlayGroundPage {
+  controlToggle = new FormControl<boolean>(true);
+}
