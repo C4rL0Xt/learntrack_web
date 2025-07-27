c6ba625ba4a77c6ca68cfbcf68b8af2a77a52f16
melani yulli laveriano porroa <melani.laveriano@unmsm.edu.pe>
2025-07-27 16:55:32 -0500

    avanza2 etiqueta

--- src/app/ui/pages/extras/playground/playground.component.ts
+++ src/app/ui/pages/extras/playground/playground.component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 import { SeccionComponent } from './components/seccion-component/seccion.component';
 import { TrackuiToggleButton } from '../../../shared/trackui/trackui-toggle-button/trackui-toggle-button.component';
 import { TrackuiEtiquetaComponent } from '../../../shared/trackui/trackui-etiqueta/trackui-etiqueta.component';
-
+import { NgFor } from '@angular/common';
+import { TiposEtiqueta } from '../../../../core/domain/types/tipos-etiqueta.type';
 @Component({
   selector: 'playground',
-  imports: [SeccionComponent, TrackuiToggleButton, TrackuiEtiquetaComponent],
+  imports: [SeccionComponent, TrackuiToggleButton, TrackuiEtiquetaComponent, NgFor],
   templateUrl: 'playground.component.html',
   styleUrl: 'playground.component.scss',
 })
-export class PlayGroundPage {}
+
+export class PlayGroundPage {
+tiposEtiquetas: TiposEtiqueta[] = [
+  'primary',
+  'secondary',
+  'tertiary',
+  'cuaternary',
+  'quinary',
+  'neutral',
+  'subordinary',
+  'warning',
+  'success',
+  'danger',
+];
+}
