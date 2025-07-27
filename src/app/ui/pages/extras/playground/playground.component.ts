b3958caae72c76996ed9ea8713a9cf531b538a63
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2025-07-27 17:32:24 -0500

    Agrega directiva icon

--- src/app/ui/pages/extras/playground/playground.component.ts
+++ src/app/ui/pages/extras/playground/playground.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SeccionComponent } from './components/seccion-component/seccion.component';
 import { TrackuiToggleButton } from '../../../shared/trackui/trackui-toggle-button/trackui-toggle-button.component';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TrackUiIconsDirective } from '../../../shared/trackui/trackui-icons/trackui-icons.directive';
 
 @Component({
   selector: 'playground',
-  imports: [SeccionComponent, ReactiveFormsModule, TrackuiToggleButton],
+  imports: [SeccionComponent, ReactiveFormsModule, TrackuiToggleButton, TrackUiIconsDirective],
   templateUrl: 'playground.component.html',
   styleUrl: 'playground.component.scss',
 })
