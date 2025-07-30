b55f5c412b01ea5064af28e628cd775ae3bc665d
melani yulli laveriano porroa <melani.laveriano@unmsm.edu.pe>
2025-07-29 22:55:24 -0500

    avanza beboton

---
+++ src/app/core/domain/types/tipos-button.type.ts
@@ -0,0 +1,12 @@
+export type TiposButton =
+	| 'primary'
+	| 'secondary'
+	| 'tertiary'
+	| 'cuaternary'
+	| 'quinary'
+	| 'outlined'
+	| 'link'
+	| 'texto';
+
+export type FormasButton = 'normal' | 'circular';
+export type PositionsIcon = 'left' | 'right';
---
+++ src/app/ui/shared/trackui/trackui-button/trackui-button.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TrackuiButtonDirective } from './trackui-button.directive';
+
+describe('TrackuiButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TrackuiButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
